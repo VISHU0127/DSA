4dbc8d2a1408bd118cde3e95ccfb9c03a82bc815
Your GitHub Username <your-github-email@example.com>
2025-12-18 23:29:25 +0000

    Sync LeetCode submission Runtime - 1211 ms (41.24%), Memory - 0.0B (100.00%)

---
+++ solutions/0185-department-top-three-salaries/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select d.name as department,
+    e1.name as Employee,
+    e1.salary as salary
+from employee as e1 inner join department d
+on e1.departmentid = d.id
+where 3 > (
+    select count(distinct (e2.salary))
+    from employee as e2
+    where e1.salary < e2.salary and 
+    e1.departmentid = e2.departmentid
+)
