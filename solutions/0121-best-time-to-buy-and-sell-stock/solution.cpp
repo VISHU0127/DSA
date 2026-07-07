4b18ce75a54179e84ac6f4e419fcf3059aa35173
Your GitHub Username <your-github-email@example.com>
2026-07-07 12:56:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 97.4 MB (61.49%)

---
+++ solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+      int bestbuy = prices[0];
+      int mp = 0;
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] > bestbuy){
+                  mp = max(mp, prices[i] - bestbuy);
+            }
+            bestbuy = min(bestbuy, prices[i]);
+        }
+      return mp;
+    }
+};
