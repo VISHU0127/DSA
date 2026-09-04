f6aa7048bcaac113254ff8d4f6b62efe9ae8dee4
Your GitHub Username <your-github-email@example.com>
2026-09-04 06:10:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26.1 MB (42.66%)

--- solutions/0042-trapping-rain-water/solution.cpp
+++ solutions/0042-trapping-rain-water/solution.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int trap(vector<int>& ht) {
         int n = ht.size();
-        vector<int> lmax(n, 0);
-        vector<int> rmax(n, 0);
-        lmax[0] = ht[0];
-        rmax[n - 1] = ht[n - 1];
-
-        for(int i = 1; i < n; i++){
-            lmax[i] = max(lmax[i - 1], ht[i]);
-        }
-        for(int i = n - 2; i >= 0; i--){
-            rmax[i] = max(rmax[i + 1], ht[i]);
-        }
-
         int ans = 0;
-        for(int i = 0; i < n; i++){
-            ans += min(lmax[i], rmax[i]) - ht[i];
+        int l = 0;
+        int r = n-1;
+        int lmax = 0;
+        int rmax = 0;
+        while(l < r){
+            lmax = max(lmax, ht[l]);
+            rmax = max(rmax, ht[r]);
+            if(lmax < rmax){
+                ans += (lmax - ht[l]);
+                l++;
+            } else{
+                ans += (rmax - ht[r]);
+                r--;
+            }
         }
     return ans;
     }
